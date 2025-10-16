f4d4582f6b85d7d53bf7ed9e8abfb6212b776e79
alfahrelrifananda <pahrel1234@gmail.com>
2025-10-16 17:44:13 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    
+    // ! 1.
+    // ? My First C Program
+    
+    // This is a comment
+    
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
+   
+    printf("I like C\n");
+    printf("Its a great language");
+    return 0;
+}
