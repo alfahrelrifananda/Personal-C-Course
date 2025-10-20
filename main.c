16a9ebceeefb123eebbe2cf07472c633d4d43a86
alfahrelrifananda <pahrel1234@gmail.com>
2025-10-20 18:27:11 +0700

    switch

--- main.c
+++ main.c
@@ -460,7 +460,7 @@ int main(){
    
     // ! 13.
     // ? TEMPERATURE CONVERSION PROGRAM
-    
+    /*
     char choice = '\0';
     float fahrenheit = 0.0f;
     float celsius = 0.0f;
@@ -486,7 +486,41 @@ int main(){
     } else {
         printf("Invalid choice please enter C or F \n");
     }
+    */
+   
+    // ! 14.
+    // ? Switch Statement
     
+    int dayOfTheWeek = 0;
+    
+    printf("Enter the day of the week (1-7): ");
+    scanf("%d", &dayOfTheWeek);
+    
+    switch (dayOfTheWeek) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid day! Please enter a number between 1 and 7\n");
+            break;
+    }
     return 0;
-    
 }
